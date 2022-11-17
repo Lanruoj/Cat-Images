2c1a9074edc1b53b58c941a79c995280157a7761
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-11-17 16:44:36 +1100

    Add beginClicked state and set to <Button> onClick - conditionally render <Button>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header } from "./components/Header";
 import { Section } from "./components/Section";
 import { Button } from "./components/Button";
 
 function App() {
+    const [beginClicked, setBeginClicked] = useState(false);
+    const beginApp = (e) => {
+        e.preventDefault();
+        setBeginClicked(true);
+    };
     return (
         <div className="App">
             <Header />
@@ -12,7 +17,11 @@ function App() {
                 body="
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             />
-            <Button text="Begin" />
+            {beginClicked ? (
+                <p>not clicked</p>
+            ) : (
+                <Button text="Begin" onClick={beginApp} />
+            )}
             {/* <Main>
                 <AboutSection />
                 <GenerateForm />
