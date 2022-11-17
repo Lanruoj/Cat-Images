d14ab31c8321678716ecf9d821db61449101c4de
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-11-17 16:51:03 +1100

    Add conditional rendering to body={prop} in Section

--- src/App.js
+++ src/App.js
@@ -13,15 +13,14 @@ function App() {
         <div className="App">
             <Header />
             <Section
-                title="Section"
-                body="
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                title="Cat Images"
+                body={
+                    beginClicked
+                        ? "Image generator"
+                        : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                }
             />
-            {beginClicked ? (
-                <p>not clicked</p>
-            ) : (
-                <Button text="Begin" onClick={beginApp} />
-            )}
+            {beginClicked ? null : <Button text="Begin" onClick={beginApp} />}
             {/* <Main>
                 <AboutSection />
                 <GenerateForm />
