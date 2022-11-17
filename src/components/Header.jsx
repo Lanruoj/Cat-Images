b67765f4f83ee414e74589188010dda87dd0ce7d
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-11-17 15:41:41 +1100

    Basic text links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,29 @@ import React from "react";
 import styled from "styled-components";
 
 const NavLink = styled.a`
-    color: red;
-    :visited {
-        color: red;
+    :visited,
+    :link {
+        margin: 0.5em;
+        font-size: 3em;
+        text-decoration: none;
+        font-family: monospace;
+        color: #222222;
     }
+`;
+
+const NavContainer = styled.nav`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
 `;
 
 function Header(props) {
     return (
-        <nav>
-            <NavLink>Menu</NavLink>
-        </nav>
+        <NavContainer>
+            <NavLink href="#">About</NavLink>
+            <NavLink href="#">Contact</NavLink>
+        </NavContainer>
     );
 }
 
