cf1b92bce9ba62fa5c52764a09b6ef1cd3ef52cf
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-11-17 16:32:14 +1100

    Create / style Button component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Header } from "./components/Header";
 import { Section } from "./components/Section";
+import { Button } from "./components/Button";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 body="
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             />
+            <Button text="Begin" />
             {/* <Main>
                 <AboutSection />
                 <GenerateForm />
---
+++ src/components/Button.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button.attrs({ type: "submit" })`
+    background-color: #b7b7b7;
+    border: none;
+    border-radius: 0.3em;
+    height: 2em;
+    width: 6em;
+    font-size: 3em;
+    font-family: monospace;
+`;
+
+const FormContainer = styled.form`
+    display: flex;
+    justify-content: center;
+`;
+
+function Button(props) {
+    return (
+        <FormContainer onClick={props.onClick}>
+            <StyledButton>{props.text}</StyledButton>
+        </FormContainer>
+    );
+}
+
+export { Button };
