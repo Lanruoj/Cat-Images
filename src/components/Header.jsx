a9c8520b5fe46b380a5af07da52641d4cbc1af5f
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-11-17 15:32:45 +1100

    Add Header scaffold component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+import styled from "styled-components";
 
+const NavLink = styled.a`
+    color: red;
+    :visited {
+        color: red;
+    }
+`;
+
+function Header(props) {
+    return (
+        <nav>
+            <NavLink>Menu</NavLink>
+        </nav>
+    );
+}
+
+export { Header };
